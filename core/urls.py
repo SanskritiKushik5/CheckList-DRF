06f07e0a7898401b79390f3954553fe2ecb0d3dd
SanskritiKushik5 <sanskritikushik23@gmail.com>
2021-04-25 00:44:16 +0530

    Basic API

--- core/urls.py
+++ core/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
-from core.views import TestAPIView, CheckListsAPIView
+from core.views import CheckListsAPIView, CheckListAPIView
 
 urlpatterns = [
-    path("", TestAPIView.as_view()),
     path("api/checklists/", CheckListsAPIView.as_view()),
+    path("api/checklist/<int:pk>/", CheckListAPIView.as_view()),
 ]
--- core/views.py
+++ core/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render
+from django.http import Http404
 from rest_framework.response import Response
 from rest_framework.views import APIView
+from rest_framework import status
 from core.models import CheckList
+from core.serializers import CheckListSerializer
 
 
 class TestAPIView(APIView):
@@ -10,6 +13,50 @@ class TestAPIView(APIView):
 
 
 class CheckListsAPIView(APIView):
+    serializer_class = CheckListSerializer
+
     def get(self, request, format=None):
         data = CheckList.objects.all()
-        return Response(data)
+
+        serializer = self.serializer_class(data, many=True)
+        serialized_data = serializer.data
+        return Response(serialized_data, status=status.HTTP_200_OK)
+
+    def post(self, request, format=None):
+        serializer = self.serializer_class(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            serialized_data = serializer.data
+            return Response(serialized_data, status=status.HTTP_201_CREATED)
+
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class CheckListAPIView(APIView):
+    serializer_class = CheckListSerializer
+
+    def get_object(self, pk):
+        try:
+            return CheckList.objects.get(pk=pk)
+        except CheckList.DoesNotExist:
+            raise Http404
+
+    def get(self, request, pk, format=None):
+        serializer = self.serializer_class(self.get_object(pk))
+        serialized_data = serializer.data
+        return Response(serialized_data, status=status.HTTP_200_OK)
+
+    def put(self, request, pk, format=None):
+        checklist = self.get_object(pk)
+        serializer = self.serializer_class(checklist, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            serialized_data = serializer.data
+            return Response(serialized_data, status=status.HTTP_200_OK)
+
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request, pk, format=None):
+        checkList = self.get_object(pk)
+        checkList.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
